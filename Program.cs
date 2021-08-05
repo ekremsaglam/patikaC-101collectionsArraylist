1855caafbefeb21e59ef967e90e664435ee149b6
Ekrem Sağlam <ekremsaglammm@gmail.com>
2021-08-05 20:46:41 +0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace collectionsArraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste =new ArrayList();
+            liste.Add("ayşe");
+            liste.Add(2);
+            liste.Add(true);
+            liste.Add('A');
+
+            //verilere erişim
+            Console.WriteLine(liste[0]);
+
+            foreach(var item in liste){
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("*********Add Range********");
+            int[] sayilar = {1,4,5,6,7,8,56,25,6,5,56,2,5,56,23,56,56,5,52};
+            liste.AddRange(sayilar);
+            foreach(var item in liste){
+                Console.WriteLine(item);
+            }
+            List<string> renkler = new List<string>();
+            renkler.Add("mavi");
+            renkler.Add("turuncu");
+            liste.AddRange(renkler);
+            Console.WriteLine("*********Sort********");
+            
+
+
+
+        }
+    }
+}
